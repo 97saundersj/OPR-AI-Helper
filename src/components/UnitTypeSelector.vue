9a7da18f9b159520c4fe84e308aabe5acc539209
97saundersj <jacques_s@hotmail.co.uk>
2022-05-05 23:31:41 +0100

    Fixed deploy issues

--- src/components/UnitTypeSelector.vue
+++ src/components/UnitTypeSelector.vue
@@ -12,7 +12,7 @@ export default {
   <div class="greetings">
     <h2>Select a Unit Type</h2>
     <div class="d-flex p-2">
-      <button class="btn btn-primary m-2" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Hybrid Unit</button>
+      <button class="btn btn-primary m-2" data-toggle="tooltip" data-placement="top" title="Test">Hybrid Unit</button>
       <button class="btn btn-primary m-2">Shooting Unit</button>
       <button class="btn btn-primary m-2">Melee Unit</button>
     </div>
