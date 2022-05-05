278d4c0252c85b5d6dd199e35acbd05bc395f1df
97saundersj <jacques_s@hotmail.co.uk>
2022-05-05 21:49:13 +0100

    Added Tooltip

--- src/components/UnitTypeSelector.vue
+++ src/components/UnitTypeSelector.vue
@@ -1,16 +1,3 @@
-<template>
-  <div class="card bg-dark p-2 hello">
-  <div class="greetings">
-    <h2>Select a Unit Type</h2>
-    <div class="d-flex p-2">
-      <button class="btn btn-primary m-2">Hybrid Unit</button>
-      <button class="btn btn-primary m-2">Shooting Unit</button>
-      <button class="btn btn-primary m-2">Melee Unit</button>
-    </div>
-  </div>
-</div>
-</template>
-
 <script>
 export default {
   name: 'HelloWorld',
@@ -19,6 +6,19 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="card bg-dark p-2 hello">
+  <div class="greetings">
+    <h2>Select a Unit Type</h2>
+    <div class="d-flex p-2">
+      <button class="btn btn-primary m-2" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Hybrid Unit</button>
+      <button class="btn btn-primary m-2">Shooting Unit</button>
+      <button class="btn btn-primary m-2">Melee Unit</button>
+    </div>
+  </div>
+</div>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
