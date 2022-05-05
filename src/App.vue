95945ecb48a8032ed693a5f270e68e69ce0d73fb
97saundersj <jacques_s@hotmail.co.uk>
2022-05-05 21:07:39 +0100

    Added demo text

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UnitTypeSelector from './components/UnitTypeSelector.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UnitTypeSelector
   }
 }
 </script>
 
+<template>
+  <HelloWorld v-if="false" msg="Welcome to Your Vue.js App"/>
+  <UnitTypeSelector></UnitTypeSelector>
+</template>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 1%;
 }
 </style>
---
+++ src/components/UnitTypeSelector.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card bg-dark p-2 hello">
+  <div class="greetings">
+    <h2>Select a Unit Type</h2>
+    <div class="d-flex p-2">
+      <button class="btn btn-primary m-2">Hybrid Unit</button>
+      <button class="btn btn-primary m-2">Shooting Unit</button>
+      <button class="btn btn-primary m-2">Melee Unit</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
