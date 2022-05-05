df8a6ba49b2a4d5563fbcec4119e347c7432a10a
97saundersj <jacques_s@hotmail.co.uk>
2022-05-06 00:56:20 +0100

    Added tooltips to all buttons

--- src/components/UnitTypeSelector.vue
+++ src/components/UnitTypeSelector.vue
@@ -3,6 +3,13 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      hybridTooltip: "Units that have melee weapons which are better than their ranged weapons.",
+      shootingTooltip: "Units that have ranged weapons which are better than their melee weapons.",
+      meleeTooltip: "Units that don’t have any ranged weapons."
+    }
   }
 }
 </script>
@@ -12,9 +19,9 @@ export default {
   <div class="greetings">
     <h2>Select a Unit Type</h2>
     <div class="d-flex p-2">
-      <button class="btn btn-primary m-2" data-toggle="tooltip" data-placement="top" title="Test">Hybrid Unit</button>
-      <button class="btn btn-primary m-2">Shooting Unit</button>
-      <button class="btn btn-primary m-2">Melee Unit</button>
+      <button class="btn btn-primary m-2" :title="hybridTooltip">Hybrid Unit</button>
+      <button class="btn btn-primary m-2" :title="shootingTooltip">Shooting Unit</button>
+      <button class="btn btn-primary m-2" :title="meleeTooltip">Melee Unit</button>
     </div>
   </div>
 </div>
