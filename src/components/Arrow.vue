aaf5cc4ae5639ad06a006f26bab4ee02709f53ee
97saundersj <34074715+97saundersj@users.noreply.github.com>
2022-05-08 12:21:18 +0000

    Deploying to gh-pages from @ 97saundersj/OPR-AI-Helper@71a5202204c2709eae9bf2da5c50b918d1d965cb 🚀

---
+++ src/components/Arrow.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'ArrowImage',
+}
+</script>
+
+<template>
+  <div>
+    <svg width="100" height="100" style="transform: rotate(90deg);" xmlns="http://www.w3.org/2000/svg" float="left" >
+      <defs>
+        <marker id="arrowhead" markerWidth="10" markerHeight="3" 
+        refX="0" refY="1.5" orient="auto" fill="lightgray">
+          <polygon points="0 0, 5 1.5, 0 3" />
+        </marker>
+      </defs>
+      <line x1="0" y1="50" x2="50" y2="50" stroke="lightgray" 
+      stroke-width="8" marker-end="url(#arrowhead)" />
+    </svg>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/UnitTypeSelector.vue
@@ -0,0 +1,44 @@
+<script>
+import HybridUnit from './HybridUnit.vue'
+import Arrow from "./Arrow.vue"
+
+export default {
+  name: 'UnitTypeSelector',
+  components: {
+    HybridUnit,
+    Arrow
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      hybridTooltip: "Units that have melee weapons which are better than their ranged weapons.",
+      shootingTooltip: "Units that have ranged weapons which are better than their melee weapons.",
+      meleeTooltip: "Units that don’t have any ranged weapons.",
+      selectedUnit: ""
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="decision-card card bg-dark">
+      <h2 class="card-header">Select a Unit Type</h2>
+      <div class="card-body">
+        <button class="btn btn-primary m-2" :title="hybridTooltip" @click="selectedUnit = 'hybrid'">Hybrid Unit</button>
+        <button class="btn btn-primary m-2" :title="shootingTooltip" @click="selectedUnit = 'shooting'">Shooting Unit</button>
+        <button class="btn btn-primary m-2" :title="meleeTooltip" @click="selectedUnit = 'melee'">Melee Unit</button>
+      </div>
+    </div>
+    <div v-if="true" class="decision-card">
+      <Arrow></Arrow>
+      <HybridUnit v-if="selectedUnit === 'hybrid'"></HybridUnit>
+    </div>
+</div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
