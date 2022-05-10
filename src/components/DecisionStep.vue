11c029a47d70d8a062b45f986ae084e99a2ab42e
97saundersj <jacques_s@hotmail.co.uk>
2022-05-10 21:28:09 +0100

    Converted All Hybrid Units choices to DecisionSteps

--- src/components/DecisionStep.vue
+++ src/components/DecisionStep.vue
@@ -1,10 +1,27 @@
 <script>
+import Arrow from "./Arrow.vue"
 
 export default {
   name: 'DecisionOption',
+  components: {
+    Arrow
+  },
   props: {
+    useArrow: {
+      type: Boolean,
+      default: true
+    },
+    requiredStepsMet: null,
     modelValue: null,
     choiceText: String,
+    resultYesText: {
+      type: String,
+      default: ''
+    },
+    resultNoText: {
+      type: String,
+      default: ''
+    },
   },
   data() {
     return {
@@ -20,16 +37,26 @@ export default {
 </script>
 
 <template>
-<div>
+<div v-if="requiredStepsMet">
+
   <div class="decision-card">
+    <Arrow v-if="requiredStepsMet && useArrow"></Arrow>
+
     <div class="card bg-dark objectives-under-control">
       <h2 class="card-header">{{ choiceText }}</h2>
+
       <div class="d-flex p-2 decision-card">
         <button class="btn m-2" :class="modelValue ? 'btn-primary' : 'btn-secondary'" @click="setDecision(true)">Yes</button>
         <button class="btn m-2" :class="modelValue === false ? 'btn-primary' : 'btn-secondary'" @click="setDecision(false)">No</button>
       </div>
+
+      <div class="card-footer">
+        <p v-if="modelValue">{{ resultYesText }}</p>
+        <p v-if="modelValue == false">{{ resultNoText }}</p>
+      </div>
     </div>
   </div>
+
 </div>
 </template>
 
--- src/components/HybridUnitDecisions.vue
+++ src/components/HybridUnitDecisions.vue
@@ -23,111 +23,56 @@ export default {
 
 <template>
   <div>
-    <!--
-    <div class="decision-card">
-      <div class="card bg-dark objectives-under-control">
-        <h2 class="card-header">Are there any objectives not under the AI’s control?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="objectiveNotUnderControl ? 'btn-primary' : 'btn-secondary'" @click="objectiveNotUnderControl = true">Yes</button>
-          <button class="btn m-2" :class="objectiveNotUnderControl === false ? 'btn-primary' : 'btn-secondary'" @click="objectiveNotUnderControl = false">No</button>
-        </div>
-      </div>
-    </div>
-    -->
     <DecisionStep 
       class="objectives-under-control"
+      :useArrow="false"
+      :requiredStepsMet="true"
       v-model="objectiveNotUnderControl"
       :choiceText="'Are there any objectives not under the AI’s control?'">
     </DecisionStep>
 
-    <div class="decision-card" v-if="objectiveNotUnderControl">
-      <Arrow></Arrow>
+    <DecisionStep 
+      class="objectives-under-control"
+      :requiredStepsMet="objectiveNotUnderControl"
+      v-model="enemiesInWay"
+      :choiceText="'Are there any enemies in the way?'"
+      :resultYesText="'Charge enemy if possible, else Advance toward objective and shoot if possible, else Rush toward objective.'">
+    </DecisionStep>
 
-      <div class="card bg-dark decision-card enemies-in-way">
-        <h2 class="card-header">Are there any enemies in the way?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="enemiesInWay === true ? 'btn-primary' : 'btn-secondary'" @click="enemiesInWay = true">Yes</button>
-          <button class="btn m-2" :class="enemiesInWay === false ? 'btn-primary' : 'btn-secondary'" @click="enemiesInWay = false">No</button>
-        </div>
+    <DecisionStep 
+      class="enemies-in-way"
+      :requiredStepsMet="objectiveNotUnderControl && enemiesInWay == false"
+      v-model="inRushRange"
+      :choiceText="'Is the objective in Rush range but not in Advance range?'"
+      :resultYesText="'Rush toward objective.'">
+    </DecisionStep>
 
-        <div class="card-footer" v-if="enemiesInWay">
-          <p>Charge enemy if possible, else Advance toward objective and shoot if possible, else Rush toward objective.</p>
-        </div>
-      </div>
-    </div>
+    <DecisionStep 
+      class="in-shooting-range"
+      :requiredStepsMet="objectiveNotUnderControl && enemiesInWay == false && inRushRange == false"
+      v-model="inShootingRange"
+      :choiceText="'If you Advance will any enemies be in shooting range?'"
+      :resultYesText="'Advance toward objective and shoot if possible.'"
+      :resultNoText="'Rush toward objective.'">
+    </DecisionStep>
 
-    <div class="decision-card" v-if="objectiveNotUnderControl && enemiesInWay == false">
-      <Arrow></Arrow>
+    <DecisionStep 
+      class="enemies-in-charge-range"
+      :requiredStepsMet="objectiveNotUnderControl == false"
+      v-model="inChargeRange"
+      :choiceText="'Are any enemies in Charge range?'"
+      :resultYesText="'Charge enemy.'">
+    </DecisionStep>
 
-      <div class="card bg-dark decision-card enemies-in-way">
-        <h2 class="card-header">Is the objective in Rush range but not in Advance range?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="inRushRange ? 'btn-primary' : 'btn-secondary'" @click="inRushRange = true">Yes</button>
-          <button class="btn m-2" :class="inRushRange === false ? 'btn-primary' : 'btn-secondary'" @click="inRushRange = false">No</button>
-        </div>
-
-        <div v-if="inRushRange">
-          <p>Rush toward objective.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="decision-card" v-if="objectiveNotUnderControl && enemiesInWay == false && inRushRange == false">
-      <Arrow></Arrow>
-
-      <div class="card bg-dark decision-card enemies-in-way">
-        <h2 class="card-header">If you Advance will any enemies be in shooting range?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="inShootingRange ? 'btn-primary' : 'btn-secondary'" @click="inShootingRange = true">Yes</button>
-          <button class="btn m-2" :class="inShootingRange === false ? 'btn-primary' : 'btn-secondary'" @click="inShootingRange = false">No</button>
-        </div>
-
-        <div v-if="inShootingRange">
-          <p>Advance toward objective and shoot if possible.</p>
-        </div>
-
-        <div v-if="inShootingRange == false">
-          <p>Rush toward objective.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="decision-card" v-if="objectiveNotUnderControl == false">
-      <Arrow></Arrow>
-
-      <div class="card bg-dark decision-card enemies-in-way">
-        <h2 class="card-header">Are any enemies in Charge range?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="inChargeRange ? 'btn-primary' : 'btn-secondary'" @click="inChargeRange = true">Yes</button>
-          <button class="btn m-2" :class="inChargeRange === false ? 'btn-primary' : 'btn-secondary'" @click="inChargeRange = false">No</button>
-        </div>
-
-        <div v-if="inChargeRange">
-          <p>Charge enemy.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="decision-card" v-if="objectiveNotUnderControl == false && inChargeRange == false">
-      <Arrow></Arrow>
-
-      <div class="card bg-dark decision-card enemies-in-way">
-        <h2 class="card-header">If you Advance will any enemies be in shooting range?</h2>
-        <div class="d-flex p-2 decision-card">
-          <button class="btn m-2" :class="inShootingRangeOfEnemy ? 'btn-primary' : 'btn-secondary'" @click="inShootingRangeOfEnemy = true">Yes</button>
-          <button class="btn m-2" :class="inShootingRangeOfEnemy === false ? 'btn-primary' : 'btn-secondary'" @click="inShootingRangeOfEnemy = false">No</button>
-        </div>
-
-        <div v-if="inShootingRangeOfEnemy">
-          <p>Advance toward enemy and shoot if possible.</p>
-        </div>
-
-        <div v-if="inShootingRangeOfEnemy == false">
-          <p>Rush toward enemy.</p>
-        </div>
-      </div>
-    </div>
-</div>
+    <DecisionStep 
+      class="advance-into-shooting-range"
+      :requiredStepsMet="objectiveNotUnderControl == false && inChargeRange == false"
+      v-model="inShootingRangeOfEnemy"
+      :choiceText="'If you Advance will any enemies be in shooting range?'"
+      :resultYesText="'Advance toward enemy and shoot if possible.'"
+      :resultNoText="'Rush toward enemy.'">
+    </DecisionStep>
+  </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
